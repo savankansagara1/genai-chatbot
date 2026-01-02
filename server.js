a97095df736f3a11216a4eceb659100fec2bac8e
savan patel <savankansagara1005@gmail.com>
2026-01-02 12:19:26 +0530

    fix: use Render assigned PORT

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import { generate } from "./chatbot.js";
 
 const app = express();
-const port = 3001;
+const PORT = process.env.PORT || 3001;
 app.use(cors());
 app.use(express.json());
 
